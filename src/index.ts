3ca03ddfe927d796646c1a98048d234fdd42612a
Cansy <cansyland@gmail.com>
2023-11-19 07:51:56 +0100

    add CORS rule

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import { mintNFT } from './mintNft';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.json());
+
+app.use(cors());
 app.use(express.json());
 
 app.post('/mintNFT', async (req, res) => {
